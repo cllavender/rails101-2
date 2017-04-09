e96410743de8a28336abb5422bbe87d4a3785798
cl <cl_fighting@163.com>
2017-04-09 10:30:03 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -42,6 +42,31 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, alert: "Group Deleted!"
   end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本论坛成功！"
+    else
+      flash[:warning] = "你已经是本论坛成员了。"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本论坛！"
+    else
+      flash[:warning] = "你不是本论坛成员。"
+    end
+    redirect_to group_path(@group)
+  end
+
 
 
 
